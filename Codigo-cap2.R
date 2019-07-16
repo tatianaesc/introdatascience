0f7c1d18ff18cddf27c52c6991476018e3b6736e
Tatiana Escovedo <tatianae@gmail.com>
2019-07-15 22:58:09 -0300

    Add files via upload

---
+++ Codigo-cap2.R
@@ -0,0 +1,94 @@
+## Criando estruturas de dados dentro do R
+
+
+### Concatenando dados no R - função c()
+
+# Dados das pessoas
+idade = c(24, 48, 32, 65, 38, 56, 74, 19, 29, 22)
+peso = c(80, 67, 49, 55, 89, 72, 45, 88, 56, 74)
+altura = c(180, 165, 162, 175, 172, 165, 168, 185, 172, 168)
+sexo = c('M','F','M','F','M','F','M','F','M','F')
+
+### Inspecionando as estruturas
+
+idade
+show(idade)
+sexo[3]
+sexo[3:7]
+sexo[c(3,5)]
+
+### Alterando valores das estruturas e listando os objetos no ambiente
+
+sexo
+sexo[c(3,5)] = c("F", "M")
+sexo
+
+a = 2+3+4+5+6+7
+rm(a)
+
+a = 2+3+4+5+6+7
+b = a*2
+resultado = a+b
+rm(a, b, resultado)
+
+ls()
+
+### Funções simples aplicadas em um objeto
+
+length(idade)
+
+idade
+sort(idade)
+sort(idade, decreasing = T)
+
+str(idade)
+str(sexo)
+
+### Operadores lógicos
+
+altura>170
+sexo[altura>170]
+table(sexo[altura>170])
+
+## Trabalhando com Data Frames
+
+### Criando um Data Frame
+
+tabela = data.frame(altura, sexo, idade, peso)
+View(tabela)
+
+
+### Manipulando um data frame
+
+
+str(tabela)
+names(tabela)
+names(tabela)[1] = "Height"
+names(tabela)[1]
+names(tabela)[1] = "altura"
+names(tabela)[1]
+
+
+head(tabela, n = 5)
+tail(tabela, n = 5)
+tabela$sexo
+head(tabela$sexo, 7) # note que não é necessário colocar n = 7 para funcionar
+tabela[1, 1] # altura da primeira pessoa
+tabela[2, 1] # altura da segunda pessoa
+tabela[1, 2] # sexo da primeira pessoa
+tabela[, 3] # dispõe as idades de todas as pessoas
+tabela[2, ] # apresenta as informações da segunda pessoa
+tabela[, c(1, 3)] # apresenta somente a 1a e 3a coluna
+tabela[, -2] # omite o sexo das pessoas 
+
+
+### Exportando/Importando um data frame
+
+
+write.csv(tabela, "BaseDados.csv")
+rm(tabela)
+tabela = read.csv("BaseDados.csv")
+View(tabela)
+tabela = tabela[, -1]
+head(tabela) # por default, mostra as 6 primeiras linhas
+
---
+++ Codigo-cap4.R
@@ -0,0 +1,81 @@
+## Importação de Dados
+
+# Lê de um arquivo ou URL e armazena o resultado em um novo data frame dados
+dados <- read.table(
+  'https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv',
+  sep=',', header=T
+)
+
+# mostra o tipo do objeto
+class(dados) 
+# mostra a distribuição de cada variável do dataset
+summary(dados) 
+# mostra quantas linhas e colunas há nos dados
+dim(dados) 
+
+dados2 <- read.table(“iris.csv”, sep=',', header=T)
+
+dados3 <- iris
+
+## Análise Exploratória
+
+# cria um histograma para cada atributo
+par(mfrow=c(1,4))
+for(i in 1:4) {
+  hist(dados[,i], main=names(dados)[i])
+}
+
+# carrega o pacote 
+library(lattice)
+# cria um gráfico de densidade para cada atributo
+par(mfrow=c(1,4))
+for(i in 1:4) {
+  plot(density(dados[,i]), main=names(dados)[i])
+}
+
+# cria um boxplot para cada atributo
+par(mfrow=c(1,4))
+for(i in 1:4) {
+  boxplot(dados[,i], main=names(dados)[i])
+}
+
+
+# instala e carrega o pacote 
+install.packages("corrplot")
+library(corrplot)
+# calcula as correlações
+correlations <- cor(dados[,1:4])
+# cria o gráfico de correlação
+corrplot(correlations, method="circle")
+
+
+# cria um gráfico de dispersão para cada um dos pares de atributos
+pairs(dados)
+
+# colore o gráfico de dispersão por classes
+pairs(species~., data=dados, col=dados$species)
+
+# instala e carrega o pacote 
+install.packages("caret")
+library(caret)
+# cria um gráfico de densidade para cada atributo, separado por classe
+x <- dados[,1:4]
+y <- dados[,5]
+escalas <- list(x=list(relation="free"), y=list(relation="free"))
+featurePlot(x=x, y=y, plot="density", scales=escalas)
+
+# cria um boxplot cada atributo, separado por classe
+featurePlot(x=x, y=y, plot="box")
+
+
+## Preparação e Limpeza
+
+# carrega os dados do banco de crédito alemão
+dados4 <- read.table('custdata.tsv',header = T,sep = '\t')
+summary(dados4)
+
+# normaliza os dados de idade dos clientes
+summary(dados4$age)
+idadeMedia <- mean(dados4$age)
+dados4$age.normalized <- dados4$age/idadeMedia
+summary(dados4$age.normalized)
